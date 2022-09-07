273a73ea02440b20b54034b6162d6b6aaf9a74b4
Naif-Deriv <112558231+Naif-Deriv@users.noreply.github.com>
2022-09-07 12:05:56 +0400

    filter chips with rx

--- lib/models/thing.dart
+++ lib/models/thing.dart
@@ -1,10 +1,17 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/foundation.dart';
 
+enum ThingType {
+  animal,
+  person,
+}
+
 @immutable
 class Thing {
   final String name;
+  final ThingType type;
   const Thing({
     required this.name,
+    required this.type,
   });
 }
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:rx_dart_turbo/bloc/search_bloc.dart';
-import 'package:rx_dart_turbo/views/search_result_view.dart';
+import 'package:rx_dart_turbo/bloc/filter_bloc.dart';
+import 'package:rx_dart_turbo/models/thing.dart';
 
-import '../bloc/api.dart';
+const things = [
+  Thing(name: 'Foo', type: ThingType.animal),
+  Thing(name: 'Tag', type: ThingType.animal),
+  Thing(name: 'Buzz', type: ThingType.animal),
+  Thing(name: 'Lucky', type: ThingType.animal),
+  Thing(name: 'Yara', type: ThingType.person),
+  Thing(name: 'Naif', type: ThingType.person),
+  Thing(name: 'Mr.Potato', type: ThingType.person),
+  Thing(name: 'Hilary', type: ThingType.person),
+];
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -12,17 +21,16 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  late final SearchBloc searchBloc;
-
+  late final FilterBloc filterBloc;
   @override
   void initState() {
-    searchBloc = SearchBloc(api: API());
+    filterBloc = FilterBloc(thingsList: things);
     super.initState();
   }
 
   @override
   void dispose() {
-    searchBloc.dispose();
+    filterBloc.dispose();
     super.dispose();
   }
 
@@ -30,20 +38,48 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Search page'),
+        title: const Text('Filter Chip with Rx'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            TextField(
-              onChanged: searchBloc.search.add,
+            StreamBuilder<ThingType?>(
+              stream: filterBloc.chosenThingType,
+              builder: (context, snapshot) {
+                return Wrap(
+                  children: ThingType.values
+                      .map(
+                        (type) => FilterChip(
+                          selectedColor: Colors.blueGrey,
+                          onSelected: (isChosen) {
+                            filterBloc.thingTypeSink
+                                .add(isChosen ? type : null);
+                          },
+                          label: Text(type.toString()),
+                          selected: snapshot.data == type,
+                        ),
+                      )
+                      .toList(),
+                );
+              },
             ),
+            Expanded(
+              child: StreamBuilder<Iterable<Thing>>(
+                stream: filterBloc.things,
+                builder: (context, snapshot) {
+                  return ListView.builder(
+                    itemCount: snapshot.hasData ? snapshot.data!.length : 0,
+                    itemBuilder: (context, index) => Text(
+                      snapshot.data!.elementAt(index).name,
+                    ),
+                  );
+                },
+              ),
+            ),
+            const TextField(),
             const SizedBox(
               height: 25,
-            ),
-            SearchResultView(
-              searchResult: searchBloc.result,
             ),
           ],
         ),
